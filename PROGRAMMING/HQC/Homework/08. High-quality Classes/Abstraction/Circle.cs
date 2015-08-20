140a63982f3b27e1a93e4da11b20e69b0a834e9c
Dzheylyan Ademova <djeni.ademova@gmail.com>
2015-08-20 21:00:39 +0300

    added HQC hw HQ classes

---
+++ PROGRAMMING/HQC/Homework/08. High-quality Classes/Abstraction/Circle.cs
@@ -0,0 +1,27 @@
+﻿namespace Abstraction
+{
+    using System;
+
+    public class Circle : Figure
+    {
+        public Circle(double radius)
+            : base(radius, radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double Radius { get; private set; }
+        
+        public override double CalcPerimeter()
+        {
+            double perimeter = 2 * Math.PI * this.Radius;
+            return perimeter;
+        }
+
+        public override double CalcSurface()
+        {
+            double surface = Math.PI * this.Radius * this.Radius;
+            return surface;
+        }
+    }
+}
---
+++ PROGRAMMING/HQC/Homework/08. High-quality Classes/Cohesion-and-Coupling/UtilsExamples.cs
@@ -0,0 +1,34 @@
+﻿namespace CohesionAndCoupling
+{
+    using System;
+
+    public class UtilsExamples
+    {
+        public static void Main()
+        {
+            Console.WriteLine(FileExtensionMethods.GetFileExtension("example"));
+            Console.WriteLine(FileExtensionMethods.GetFileExtension("example.pdf"));
+            Console.WriteLine(FileExtensionMethods.GetFileExtension("example.new.pdf"));
+
+            Console.WriteLine(FileExtensionMethods.GetFileNameWithoutExtension("example"));
+            Console.WriteLine(FileExtensionMethods.GetFileNameWithoutExtension("example.pdf"));
+            Console.WriteLine(FileExtensionMethods.GetFileNameWithoutExtension("example.new.pdf"));
+
+            Console.WriteLine(
+                "Distance in the 2D space = {0:f2}",
+                Distance.CalcDistance2D(1, -2, 3, 4));
+            Console.WriteLine(
+                "Distance in the 3D space = {0:f2}",
+                Distance.CalcDistance3D(5, 2, -1, 3, -6, 4));
+
+            GeometryUtils.Width = 3;
+            GeometryUtils.Height = 4;
+            GeometryUtils.Depth = 5;
+            Console.WriteLine("Volume = {0:f2}", GeometryUtils.CalcVolume());
+            Console.WriteLine("Diagonal XYZ = {0:f2}", GeometryUtils.CalcDiagonalXYZ());
+            Console.WriteLine("Diagonal XY = {0:f2}", GeometryUtils.CalcDiagonalXY());
+            Console.WriteLine("Diagonal XZ = {0:f2}", GeometryUtils.CalcDiagonalXZ());
+            Console.WriteLine("Diagonal YZ = {0:f2}", GeometryUtils.CalcDiagonalYZ());
+        }
+    }
+}
