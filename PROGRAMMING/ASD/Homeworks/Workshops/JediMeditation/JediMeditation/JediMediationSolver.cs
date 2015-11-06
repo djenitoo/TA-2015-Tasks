f0345e850ffe6da3ea033d2a50c3fbb654ebab67
Dzheylyan Ademova <djeni.ademova@gmail.com>
2015-11-06 03:04:19 +0200

    Added first DSA workshop solution - Jedi Mediation

---
+++ PROGRAMMING/ASD/Homeworks/Workshops/JediMeditation/JediMeditation/JediMediationSolver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JediMeditation
+{
+    class JediMediationSolver
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            var keys = new char[] { 'm', 'k', 'p'};
+            var jediCollection = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var jediMeditiationList = new List<string>();
+
+            for (int i = 0; i < keys.Length; i++)
+            {
+                for (int j = 0; j < jediCollection.Length; j++)
+                {
+                    if (keys[i] == jediCollection[j][0])
+                    {
+                        jediMeditiationList.Add(jediCollection[j]);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", jediMeditiationList));
+        }
+    }
+}
