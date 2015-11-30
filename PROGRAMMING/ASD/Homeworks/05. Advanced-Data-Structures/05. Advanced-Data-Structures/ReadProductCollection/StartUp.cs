2fd2f476c1cc362b1274ff896bcc80dca266ebd3
Dzheylyan Ademova <djeni.ademova@gmail.com>
2015-11-30 20:21:55 +0200

    Added DSA HW Advanced-Data-Structures

---
+++ PROGRAMMING/ASD/Homeworks/05. Advanced-Data-Structures/05. Advanced-Data-Structures/ReadProductCollection/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace ReadProductCollection
+{
+    using Wintellect.PowerCollections;
+
+    class StartUp
+    {
+        static void Main(string[] args) //thanks to dentia!!
+        {
+            var products = new OrderedMultiDictionary<decimal, string>(false);
+
+            for (int i = 0; i < 500000; i++)
+            {
+                products.Add(i, "Item#" + i);
+            }
+            var output = new StringBuilder();
+            for (int i = 0; i < 10000; i++)
+            {
+                var res = products.Range(i, true, 50*i, true).Take(20);
+                output.AppendLine(string.Join(", ", res.Select(el=>el.Value))).AppendLine();
+            }
+
+            Console.WriteLine(output.ToString());
+        }
+    }
+}
