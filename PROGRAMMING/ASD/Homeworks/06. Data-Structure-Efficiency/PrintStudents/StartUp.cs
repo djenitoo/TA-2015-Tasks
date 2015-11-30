53f6f42a6842158ce07acce8bfe36bf381020dac
Dzheylyan Ademova <djeni.ademova@gmail.com>
2015-11-30 21:44:57 +0200

    Added DSA HW Data-Structure-Efficienty

---
+++ PROGRAMMING/ASD/Homeworks/06. Data-Structure-Efficiency/PrintStudents/StartUp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PrintStudents
+{
+    class StartUp
+    {
+        struct Student
+        {
+            public string firstName;
+            public string lastName;
+        }
+
+        static void Main(string[] args)
+        {
+            var dataBase = new SortedDictionary<string,List<Student>>();
+            foreach (string line in File.ReadLines(@"..\\..\\students.txt"))
+            {
+                string[] words = line.Split(new char[] {' ', '|'}, StringSplitOptions.RemoveEmptyEntries);
+                var currentStudent = new Student();
+                currentStudent.firstName = words[0];
+                currentStudent.lastName = words[1];
+                var subject = words[2];
+                if (dataBase.ContainsKey(subject))
+                {
+                    dataBase[subject].Add(currentStudent);
+                }
+                else
+                {
+                    dataBase.Add(subject, new List<Student>() { currentStudent });
+                }
+            }
+
+            foreach (var key in dataBase.Keys)
+            {
+                Console.Write("{0}: ", key);
+                dataBase[key].Sort((emp1, emp2) => emp1.firstName.CompareTo(emp2.lastName));
+
+                for (int i = 0; i < dataBase[key].Count; i++)
+                {
+                    Console.Write(dataBase[key][i].firstName + " " + dataBase[key][i].lastName);
+                    if (i != dataBase[key].Count - 1)
+                    {
+                        Console.Write(", ");
+                    }
+                }
+                
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ PROGRAMMING/ASD/Homeworks/06. Data-Structure-Efficiency/TraverseCompanyArticles/StartUp.cs
@@ -0,0 +1,51 @@
+﻿/*
+A large trade company has millions of articles, each described by barcode, vendor, title and price.
+
+    Implement a data structure to store them that allows fast retrieval of all articles in given price range [x…y].
+    Hint: use OrderedMultiDictionary<K,T> from Wintellect's Power Collections for .NET.
+*/
+namespace TraverseCompanyArticles
+{
+    using System;
+    using System.Linq;
+    using Wintellect.PowerCollections;
+
+    public class Item : IComparable<Item>
+    {
+        public string title { get; set; }
+        public string vendor { get; set; }
+        public string barcode { get; set; }
+        public decimal price { get; set; }
+
+        public int CompareTo(Item other)
+        {
+            return String.Compare(this.barcode, other.barcode, StringComparison.Ordinal);
+        }
+    }
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var database = new OrderedMultiDictionary<decimal, Item>(false);
+
+            for (int i = 1; i < 1000; i++)
+            {
+                var entry = new Item()
+                {
+                    price = i + (20 * i % 5),
+                    title = "Item#"+i,
+                    vendor = "Vendor" + i
+                };
+                entry.barcode = entry.GetHashCode().ToString();
+
+                database.Add(i,entry);
+            }
+
+            var result = database.Range(20, true, 1000, true).Select(ent=>ent.Key);
+
+            Console.WriteLine(string.Join(", ", result));
+
+        }
+    }
+}
